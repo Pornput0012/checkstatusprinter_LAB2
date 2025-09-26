f6e30bda89665b6faf38f8d37b1d7f10bb38b237
Your Name <you@example.com>
2025-09-26 12:07:22 +0700

    fix bug volume

--- redeploy.sh
+++ redeploy.sh
@@ -1,6 +1,3 @@
 docker rm -f check_print && \
 docker build -t check_printer:1.0 . && \
-docker run --name check_print \
-  -d -p 9090:8000 \
-  -v "$(pwd)/data:/app/data" \
-  check_printer:1.0
+docker run -d -p 9090:8000 -v ./users.db:/app/users.db check_printer:1.0
